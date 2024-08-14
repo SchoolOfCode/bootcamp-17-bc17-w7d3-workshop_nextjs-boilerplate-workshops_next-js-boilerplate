a18a3f5c809618b81e75acf83a0d31c698fb9499
Marco Di Summa <mdisumma@yahoo.com>
2024-08-14 15:05:56 +0100

    map iteration

--- components/main/howItWorks/howItWorks.js
+++ components/main/howItWorks/howItWorks.js
@@ -1,11 +1,32 @@
 import Card from "./card/card";
 
+const contents = [
+  {
+    title: "Give a a call",
+    description:
+      "Call us and book in a 'Design Consultation' on a date and time that suits you.",
+  },
+  {
+    title: "We come to you",
+    description:
+      "We come to your home to do an assessment of the space and to your style preference.",
+  },
+  {
+    title: "We reccomend",
+    description: "We send you a bespoke set of fireplace recommendations.",
+  },
+];
+
 function HowItWorks() {
   return (
     <section id="how-it-works" className="how-it-works-section">
       <h2 className="how-it-works-text">How It Works</h2>
       <div className="tiles">
-        <Card
+        {contents.map((content) => (
+          <Card title={content.title} description={content.description} />
+        ))}
+
+        {/* <Card
           title="Give a a call"
           description="Call us and book in a 'Design Consultation' on a date and time that suits you."
         />
@@ -17,7 +38,7 @@ function HowItWorks() {
         <Card
           title="We reccomend"
           description="We send you a bespoke set of fireplace recommendations."
-        />
+        /> */}
       </div>
     </section>
   );
