0b88939604e7776cf5faf958efcf4444d88c15d1
Marco Di Summa <mdisumma@yahoo.com>
2024-08-16 08:22:26 +0100

    review component pseudocode

---
+++ components/review/review.js
@@ -0,0 +1,28 @@
+//import use state
+
+function Review() {
+  // use state hock
+
+  //fetch data
+  //parse data
+  //store data (useState variable)
+
+  //clickHandler function
+
+  return;
+  // <container>
+  //title
+  //description
+  //button(england)
+  //button(Wales)
+  //button(Scotland)
+  // </container>
+
+  // conditional rendering (if selected)
+
+  // <container>
+  //display data
+  // </container>
+}
+
+export default Review; // to main element ->
