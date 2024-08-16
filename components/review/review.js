da30bd720612f026e1800516985176b88a856a87
Marco Di Summa <mdisumma@yahoo.com>
2024-08-16 11:30:50 +0100

    active class

--- components/review/review.js
+++ components/review/review.js
@@ -1,11 +1,9 @@
-'use client';
-import { useState, useEffect } from 'react'; 
-
+"use client";
+import { useState, useEffect } from "react";
 
 function Review() {
   const [selectedCountry, setSelectedCountry] = useState(null);
-  const [countryData, setCountryData] = useState([]); 
-  const [activeClass, setActiveClass] = useState(""); 
+  const [countryData, setCountryData] = useState([]);
   // https://legacy.reactjs.org/docs/hooks-state.html
   // useState hock
   // set initial value to null
@@ -15,15 +13,15 @@ function Review() {
   // https://www.w3schools.com/jsref/api_fetch.asp
   //fetch data
 
-
   useEffect(() => {
-    fetch(`https://seal-app-336e8.ondigitalocean.app/reviews?country=${selectedCountry}`)
-  .then(response => response.json())
-  .then(data => setCountryData(data));
-  console.log(`This country is selected ${selectedCountry}`);
+    fetch(
+      `https://seal-app-336e8.ondigitalocean.app/reviews?country=${selectedCountry}`
+    )
+      .then((response) => response.json())
+      .then((data) => setCountryData(data));
+    console.log(`This country is selected ${selectedCountry}`);
     console.log(countryData);
   }, [selectedCountry]);
-
 
   //parse data
   //store data (useState variable)
@@ -32,52 +30,84 @@ function Review() {
   //clickHandler function
   function clickHandler(value) {
     setSelectedCountry(value);
-    setActiveClass("active");
   }
 
   //store value
   //change color for selected
 
   return (
-  // https://files.slack.com/files-pri/T072XV4SXB9-F07H514KR7W/image20240815141956.png
-  // https://files.slack.com/files-pri/T072XV4SXB9-F07H2G5GRC2/image20240815142009.png
-  // https://files.slack.com/files-pri/T072XV4SXB9-F07H517FC4U/image20240815142040.png
-  // https://files.slack.com/files-pri/T072XV4SXB9-F07GVT1HYBG/image20240815142621.png
-  //
-<>
-  <section className="reviewContainer">
-  <h2 className="reviewTitle"> Trusted </h2>
-  <p className="reviewDesc"> We've got thousends of happy customers all over the UK. Choose your country to see the latest review.</p>
-  <div className="reviewButtonContainer"> 
-  <button onClick={() => {clickHandler("England")}} className= {`reviewButton ${""}`} > England </button>
-  <button onClick={() => {clickHandler("Wales")}} className={`reviewButton ${""}`}> Wales </button>
-  <button onClick={() => {clickHandler("Scotland")}} className={`reviewButton ${""}`}> Scotland </button>
-  </div>
-  </section>
+    // https://files.slack.com/files-pri/T072XV4SXB9-F07H514KR7W/image20240815141956.png
+    // https://files.slack.com/files-pri/T072XV4SXB9-F07H2G5GRC2/image20240815142009.png
+    // https://files.slack.com/files-pri/T072XV4SXB9-F07H517FC4U/image20240815142040.png
+    // https://files.slack.com/files-pri/T072XV4SXB9-F07GVT1HYBG/image20240815142621.png
+    //
+    <>
+      <section className="reviewContainer">
+        <h2 className="reviewTitle"> Trusted </h2>
+        <p className="reviewDesc">
+          {" "}
+          We've got thousends of happy customers all over the UK. Choose your
+          country to see the latest review.
+        </p>
+        <div className="reviewButtonContainer">
+          <button
+            onClick={() => {
+              clickHandler("England");
+            }}
+            className={`reviewButton ${
+              selectedCountry === "England" ? "active" : ""
+            }`}
+          >
+            {" "}
+            England{" "}
+          </button>
+          <button
+            onClick={() => {
+              clickHandler("Wales");
+            }}
+            className={`reviewButton ${
+              selectedCountry === "Wales" ? "active" : ""
+            }`}
+          >
+            {" "}
+            Wales{" "}
+          </button>
+          <button
+            onClick={() => {
+              clickHandler("Scotland");
+            }}
+            className={`reviewButton ${
+              selectedCountry === "Scotland" ? "active" : ""
+            }`}
+          >
+            {" "}
+            Scotland{" "}
+          </button>
+        </div>
+      </section>
 
-  <section className = "conditionalContainer">
-  <p className = "conditionalText"> {countryData.text} </p>
-  <p className = "conditionalInfo"> 
-    <span>{countryData.author}</span> <span> &nbsp;  - &nbsp; </span>
-  <span>{countryData.location}</span>
-  </p>
-   </section>
-   </>
-   )
+      <section className="conditionalContainer">
+        <p className="conditionalText"> {countryData.text} </p>
+        <p className="conditionalInfo">
+          <span>{countryData.author}</span> <span> &nbsp; - &nbsp; </span>
+          <span>{countryData.location}</span>
+        </p>
+      </section>
+    </>
+  );
 }
 
 export default Review; // to main element ->
 
-
 // author
-// : 
+// :
 // "Amy Mcdonald"
 // businessName
-// : 
+// :
 // "Fireplace Palace"
 // location
-// : 
+// :
 // "Inverness"
 // text
-// : 
-// "We couldn't be 
+// :
+// "We couldn't be
