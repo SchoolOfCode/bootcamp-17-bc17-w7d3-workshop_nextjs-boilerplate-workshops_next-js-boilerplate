91d2c1b648b73f4a1c70a1986fa9cc5943fe9b7c
Marco Di Summa <mdisumma@yahoo.com>
2024-08-20 13:41:51 +0100

    implementing switch loop

--- components/contactForm/contactForm.js
+++ components/contactForm/contactForm.js
@@ -1,63 +1,78 @@
-'use client'
+"use client";
 import { useState } from "react";
 export default function ContactForm() {
+  const [nameState, setNameState] = useState("");
 
-  const [ nameState, setNameState]= useState("");
-
-  function handleNameChange (value) {
-    setNameState (value.target.value)
-    // console.log(value);
-    // console.log(value.target.name)
-
-    
-  }
-  
-  if (value.target.name === "fullName") 
-  setNameState(value.target.value)
-
-    switch (value) {
-        case 1:
-            console.log("i is 1");
-            break;
-        case 2:
-            console.log("i is 2");
-            break;
-        case 3:
-            console.log("i is 3");
-            break;
-        case 4:
-            console.log("i is 4");
-            break;
-        case 5:
-            console.log("i is 5");
-            break;
-        default:
-            console.log("This will not be executed");
-            break;
+  function handleNameChange(e) {
+    console.log(e.target.value);
+    console.log(e.target.name);
+    switch (e) {
+      case value.target.name === "fullName":
+        setNameState(e.target.value);
+        break;
+      case value.target.name === "postCode":
+        // update
+        break;
+      case value.target.name === "addressNumber":
+        // update
+        break;
+      case value.target.name === "cityName":
+        // update
+        break;
+      default:
+        console.log("the field is empty");
+        break;
     }
+  }
 
+  // if (value.target.name === "fullName")
+  // setNameState(value.target.value)
 
-
-
-  console.log(nameState)
+  console.log(nameState);
 
   return (
-    <> 
+    <>
       <h2>Design Booking</h2>
       <form>
         <fieldset>
           <legend>Personal Information:</legend>
-          <label>Full Name<input  type="text" name="fullName" onChange={(event) => {handleNameChange(event)}} /></label>
-          <label>Postcode<input type="text" name="postcode"/></label>
-          <label>House/Flat Number and Street Name<input type="Text" name="addressNumber"/></label>
-          <label>City<input name="cityName" type="Text"/></label>      
+          <label>
+            Full Name
+            <input
+              type="text"
+              name="fullName"
+              onChange={(event) => {
+                handleNameChange(event);
+              }}
+            />
+          </label>
+          <label>
+            Postcode
+            <input type="text" name="postcode" />
+          </label>
+          <label>
+            House/Flat Number and Street Name
+            <input type="Text" name="addressNumber" />
+          </label>
+          <label>
+            City
+            <input name="cityName" type="Text" />
+          </label>
         </fieldset>
         <fieldset>
           <legend>Contact Information:</legend>
-          <label>Phone Number<input name="phoneNumber" type="number"/></label>
-          <label>Email Address<input name="emailAddress" type="email"/></label>
+          <label>
+            Phone Number
+            <input name="phoneNumber" type="number" />
+          </label>
+          <label>
+            Email Address
+            <input name="emailAddress" type="email" />
+          </label>
         </fieldset>
-        <button type="submit" value="submit">Submit</button>
+        <button type="submit" value="submit">
+          Submit
+        </button>
       </form>
     </>
   );
