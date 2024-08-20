060c42f5244055e44bf9d7b7bdb7e2638f180b3d
dom-m17 <dominicmaynard00@gmail.com>
2024-08-20 12:20:21 +0100

    Full name state handler

--- app/bookings/page.js
+++ app/bookings/page.js
@@ -1,65 +1,11 @@
-export default function () {
+import ContactForm from "./contactForm/contactForm";
+
+export default function Bookings() {
   return (
     <>
       <h1>Design Booking</h1>
 
-      <form>
-
-        <fieldset>
-
-          <legend>Personal Information:</legend>
-
-          <ul>
-
-            <li>
-              <label>Full name</label>
-              <input></input>
-            </li>
-
-            <li>
-              <label>Postcode</label>
-              <input></input>
-            </li>
-
-            <li>
-              <label>House Number</label>
-              <input></input>
-            </li>
-
-            <li>
-              <label>City</label>
-              <input></input>
-            </li>
-
-          </ul>
-
-        
-        </fieldset>
-        
-        <fieldset>
-
-          <legend>Contact Information:</legend>
-
-          <ul>
-
-            <li>
-              <label>Phone number</label>
-              <input></input>
-            </li>
-
-            <li>
-              <label>Email address</label>
-              <input></input>
-            </li>
-          </ul>
-        </fieldset>
-
-        <div>Error message to conditionally render if information is missing</div>
-
-        <button>Submit</button>
-
-      </form>
-
+      <ContactForm />
 
     </>
   );
