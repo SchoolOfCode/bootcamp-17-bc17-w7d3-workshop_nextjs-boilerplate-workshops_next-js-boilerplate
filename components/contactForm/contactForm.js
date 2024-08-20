6663a8bc5a10edc6e092ab7735d666b79881198e
Ian Smythe <ianbsmythe@googlemail.com>
2024-08-20 13:30:48 +0100

    contact form implementation

--- components/contactForm/contactForm.js
+++ components/contactForm/contactForm.js
@@ -1,44 +1,35 @@
+'use client'
 import { useState } from "react";
 export default function ContactForm() {
-  const [formData, setFormName] = useState({ name: "", age: "" });
 
-  const formDatas = {
-    name: "",
-    age: "",
-  };
+  const [ nameState, setNameState]= useState("");
 
-  console.log(formData);
-
-  function handleOnChange(e) {
-    // setage(e.target.value);
-    console.log(e.target.value);
+  function handleNameChange (value) {
+    setNameState (value.target.value)
+    console.log(value);
+    console.log(value.target.name)
   }
+  
+  console.log(nameState)
 
   return (
-    <form>
-      <fieldset>
-        <legend>title</legend>
-        <div>
-          <label>
-            {" "}
-            name
-            <input
-              onChange={(e) => {
-                handleOnChange(e);
-              }}
-              type="text"
-            />
-          </label>
-        </div>
-
-        <div>
-          <label>
-            {" "}
-            age
-            <input onChange={handleOnChange} type="text" />
-          </label>
-        </div>
-      </fieldset>
-    </form>
+    <> 
+      <h2>Design Booking</h2>
+      <form>
+        <fieldset>
+          <legend>Personal Information:</legend>
+          <label>Full Name<input  type="text" name="fullName" onChange={(event) => {handleNameChange(event)}} /></label>
+          <label>Postcode<input type="text" name="postcode"/></label>
+          <label>House/Flat Number and Street Name<input type="Text" name="addressNumber"/></label>
+          <label>City<input name="cityName" type="Text"/></label>      
+        </fieldset>
+        <fieldset>
+          <legend>Contact Information:</legend>
+          <label>Phone Number<input name="phoneNumber" type="number"/></label>
+          <label>Email Address<input name="emailAddress" type="email"/></label>
+        </fieldset>
+        <button type="submit" value="submit">Submit</button>
+      </form>
+    </>
   );
 }
